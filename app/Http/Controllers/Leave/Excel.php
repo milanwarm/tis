05e8316343f0b6f1839661610e46fd43c2caaab5
Toby <987082332@qq.com>
2018-10-07 12:36:14 +0800

    节假日导出表格功能优化

--- app/Http/Controllers/Leave/Excel.php
+++ app/Http/Controllers/Leave/Excel.php
@@ -10,14 +10,17 @@ namespace App\Http\Controllers\Leave;
 
 
 use App\Exports\HolidayLeaveExport;
+use App\Http\Config\ComConf;
+use App\Util\Logger;
 use Illuminate\Support\Facades\Validator;
+use src\ApiHelper\ApiResponse;
 use src\Exceptions\ParamValidateFailedException;
 
 class Excel {
 
     /**
      * 导出节假日信息至excel
-     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @return string
      * @throws ParamValidateFailedException
      */
     public function exportHolidayLeave(){
@@ -28,7 +31,13 @@ class Excel {
             throw new ParamValidateFailedException($validator);
         }
         $id = $params['id'];
-        return (new HolidayLeaveExport($id))->download('节假日信息.xlsx');
+        $export = new HolidayLeaveExport($id);
+        try{
+            $path = ComConf::HOST . 'storage/' . $export->store('节假日信息.xlsx','public');
+        }catch (\Exception $e){
+            Logger::fatal('leave|save_holiday_excel_failed|id:' . $id);
+        }
+        return ApiResponse::responseSuccess($path);
     }
 
 }
